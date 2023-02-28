1b15b7bd626442fd57d18a27a1d41cd71caa9d31
AliveOrdinary <19sama18@uohyd.ac.in>
2023-02-28 12:48:59 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">A killer Portfolio website</h1>
+      <p data-ns-test="project-description">
+        I want to design and build a beautiful portfolio website after learning
+        React
+      </p>
+    </div>
+  );
+};
 
 export default App;
